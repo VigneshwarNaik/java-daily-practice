8dbd092ecf99b6d57ff8efbc8b329ade421c2514
Vigneshwar Naik <vighneshwarnaik603@gmail.com>
2026-04-03 13:58:29 +0530

    Implement first repeating element using HashSet

---
+++ 01-core-java/Collection/HashSet/FirstRepeating.java
@@ -0,0 +1,28 @@
+package dsa.hashset;
+
+//Find the first repeating element in an array.
+
+import java.util.HashSet;
+
+public class FirstRepeating {
+
+	public static void main(String[] args) {
+		int[] arr={5,3,4,3,5,6};
+		
+		if(arr==null || arr.length==0) {
+			System.out.println("Invalid input");
+			return;
+		}
+		
+		HashSet<Integer> set = new HashSet<>();
+		
+		for(int num:arr) {
+			if(set.contains(num)) {
+				System.out.println("First Repeating element is : "+num);
+				return;
+			}			set.add(num);
+		}
+		System.out.println("No repeating element");
+	}
+
+}
